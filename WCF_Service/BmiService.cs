309aa817824e8b41472f5da4519005ad3fe1b31f
Bjorsch <johanbjork47@hotmail.com>
2016-01-19 11:38:42 +0100

    Skapat SelfService
    :+1:

--- WCF_Service/BmiService.cs
+++ WCF_Service/BmiService.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WCF_Service
 {
-    class BmiService
+    [ServiceContract(Namespace = "EC.WCF_Service")]
+    public interface IBMI
     {
+        [OperationContract]
+        decimal BMI(decimal height, decimal weight);
+    }
+
+    public class BmiService : IBMI
+    {
+        public decimal BMI(decimal height, decimal weight)
+        {
+            Console.WriteLine("Mottaget");
+            return weight/(height*height);
+        }
     }
 }
--- Wcf_Labb2/Program.cs
+++ Wcf_Labb2/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using WCF_Service;
 
 namespace Wcf_Labb2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_Service");
+            ServiceHost selfServiceHost = new ServiceHost(typeof(BmiService), baseAddress);
+
+            try
+            {
+                selfServiceHost.AddServiceEndpoint(
+                    typeof(IBMI), //c
+                    new WSHttpBinding(), // b
+                    "BmiService" //a
+                    );
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+
+                selfServiceHost.Description.Behaviors.Add(smb);
+
+                selfServiceHost.Open(); // Nu är tjänsten öppen
+                Console.WriteLine("Nu är tjänsten öppen");
+
+
+            }
+            finally 
+            {               
+                Console.WriteLine("Tryck på ENTER för att stänga tjänsten");
+                Console.ReadLine();
+                selfServiceHost.Close();
+            }
         }
     }
 }
