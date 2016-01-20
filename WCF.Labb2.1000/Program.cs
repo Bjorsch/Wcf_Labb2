fb8550ab604272be551ad556a8ddf6e523dd2dc4
Bjorsch <johanbjork47@hotmail.com>
2016-01-20 12:26:35 +0100

    fixat med uppgift tre

---
+++ WCF.Labb2.1000/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WCF_Service_1000;
+
+namespace WCF.Labb2._1000
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_Service_1000");
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(Birthday1000Service), baseAddress))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint(
+                        typeof(IBirthday),
+                        new WSHttpBinding(),
+                        "Birthday1000Service");
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+                    selfServiceHost.Open();
+                    Console.WriteLine("Nu är tjänsten öppen");
+
+                    Console.WriteLine("Tryck på ENTER för att stänga tjänsten");
+                    Console.ReadLine();
+                }
+                catch (CommunicationException exception)
+                {
+                    Console.WriteLine($"Ett fel inträffade {exception.Message}");
+                    selfServiceHost.Abort();
+                    Console.ReadLine();                   
+                }
+            }
+        }
+    }
+}
---
+++ WCF_Service_1000/Birthday1000Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Service_1000
+{
+    [ServiceContract(Namespace = "WCF_Service_1000")]
+    public interface IBirthday
+    {
+        [OperationContract]
+        double To1000(int year, int month, int day);
+    }
+
+    public class Birthday1000Service :IBirthday
+    {
+        public double To1000(int year, int month, int day)
+        {
+            Console.WriteLine("Mottaget");
+            var today = DateTime.Now;
+            var birthday = new DateTime(year, month, day);
+            var calc = 1000 - (today - birthday).Days % 1000;
+            return calc;
+        }
+    }
+}
