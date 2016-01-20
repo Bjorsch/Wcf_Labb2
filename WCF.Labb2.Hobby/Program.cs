3e963047e66e239b556a933acdb8235cd9d6be7b
Bjorsch <johanbjork47@hotmail.com>
2016-01-20 14:01:17 +0100

    Fixat med min hobby :movie_camera:

---
+++ WCF.Labb2.Hobby/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WSF_Service_Hobby;
+
+namespace WCF.Labb2.Hobby
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_Service_Hobby");
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(HobbyService), baseAddress ))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint(
+                        typeof (IHobby),
+                        new WSHttpBinding(),
+                        "HobbyService");
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+                    selfServiceHost.Open();
+                    Console.WriteLine("Nu är tjänsten öppen");
+
+                    Console.WriteLine("Tryck på ENTER för att stänga tjänsten");
+                    Console.ReadLine();
+                }
+                catch (CommunicationException exception)
+                {
+                    Console.WriteLine($"Ett fel inträffade {exception.Message}");
+                    selfServiceHost.Abort();
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ WSF_Service_Hobby/HobbyService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSF_Service_Hobby
+{
+    [ServiceContract (Namespace = "WSF_Service_Hobby")]
+    public interface IHobby
+    {
+        [OperationContract]
+        string MovieFromYear(string textBox);
+
+        string ButtonFromYear(string value);
+    }
+    public class HobbyService : IHobby
+    {
+        public string MovieFromYear(string textBox)
+        {
+            Console.WriteLine("Mottaget");
+            var textFile = System.IO.File.ReadAllLines(@"C:\Skola\WCF\GitHub\Wcf_Labb2\WSF_Service_Hobby\Movies.txt");
+            var text = "";
+            var theTrim = textBox.Replace(" ", "");
+            for (int i = 0; i < textFile.Length; i++)
+            {
+                var movie = textFile[i].Replace(" ", "").Split(',');
+                if (movie.Contains(theTrim))
+                {
+                    text = movie[0];
+                }
+            }
+            return text;
+        }
+        public string ButtonFromYear(string value)
+        {
+            Console.WriteLine("Mottaget");
+            var textFile = System.IO.File.ReadAllLines(@"C:\Skola\WCF\GitHub\Labb1\MyWeather\Movies.txt");
+            var text = "";
+
+            for (int i = 0; i < textFile.Length; i++)
+            {
+                var movie = textFile[i].Split(',');
+                if (movie.Contains(value))
+                {
+                    text += movie[1] + "\r\n";
+                }
+            }
+            return text;
+        }
+    }
+}
